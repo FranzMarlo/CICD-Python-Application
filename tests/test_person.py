1cfd54749b033eecc2d8d6d661dd8f46903f4622
Franz Marlo Andal <andalfranzb@gmail.com>
2025-02-06 09:02:22 +0800

    Update test_person.py with younger age and adjusted test assertions

--- tests/test_person.py
+++ tests/test_person.py
@@ -4,11 +4,11 @@ import pytest
 
 @pytest.fixture()
 def person():
-    return Person("John Doe", 30, jobs=["Software Developer"])
+    return Person("John Doe", 21, jobs=["Software Developer"])
 
 def test_init(person: Person):
     assert person.name == "John Doe"
-    assert person.age == 30
+    assert person.age == 21
     assert person.jobs == ["Software Developer"]
     
 def test_forename(person: Person):
@@ -23,7 +23,7 @@ def test_no_surname(person: Person):
     
 def test_celebrate_birthday(person: Person):
     person.celebrate_birthday()
-    assert person.age == 31
+    assert person.age == 22
 
 def test_add_job(person: Person):
     person.add_job("Software Tester")
